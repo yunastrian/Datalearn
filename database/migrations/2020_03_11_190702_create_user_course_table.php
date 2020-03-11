a1b0bef0d5dd987e176ea3125ea9d28b58dc8a0b
Kurniandha Sukma Yunastrian <13516106@std.stei.itb.ac.id>
2020-03-12 02:21:40 +0700

    add migration

---
+++ database/migrations/2020_03_11_190702_create_user_course_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserCourseTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_course', function (Blueprint $table) {
+            $table->bigInteger('id_user')->unsigned();
+            $table->bigInteger('id_course')->unsigned();
+            $table->string('role');
+
+            $table->primary(['id_user', 'id_course']);
+
+            $table->foreign('id_user')->references('id')->on('users');
+            $table->foreign('id_course')->references('id')->on('courses');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_course');
+    }
+}
